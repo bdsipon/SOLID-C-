447513595679b24b11df04748bc9af7bd6b34a8f
bdsipon <sipon.here@gmail.com>
2017-11-06 23:29:42 +1000

    Add project files.

---
+++ UserManagementSystem.Application/UserManagement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UserManagementSystem.Model;
+using UserManagementSystem.Model.Repositories;
+
+namespace UserManagementSystem.Application
+{
+    public class UserManagement
+    {
+        public bool Register(User user)
+        {
+            //User Validation
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(user.Email);
+            if ( string.IsNullOrEmpty(user.Password) || user.Password.Length < 8 || !match.Success)
+            {
+                return false;
+            }
+
+            //Save User
+            UserRepository userRepository = new UserRepository();
+            userRepository.SaveUser(user);
+
+            //Send registration email
+            MailMessage mail = new MailMessage("thiscourse@isawesome.com", user.Email);
+            SmtpClient client = new SmtpClient();
+            client.Port = 25;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Host = "smtp.google.com";
+            mail.Subject = "You have successfully registered!";
+            mail.Body = "Hope you are enjoying this course so far";
+            client.Send(mail);
+
+            return true;
+        }
+    }
+}
---
+++ UserManagementSystem.Model/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagementSystem.Model.Repositories
+{
+    public class UserRepository
+    { 
+        public void SaveUser(User user)
+        {
+           
+        }
+    }
+}
