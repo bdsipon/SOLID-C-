6e3d6f6e6878266e3b566a3cc2144a33f4532ce1
bdsipon <sipon.here@gmail.com>
2017-11-07 12:00:22 +1000

    String

---
+++ BasicToAdvance/Operation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicToAdvance
+{
+    class Operation
+    {
+        public void Oopeation()
+        {
+            string someText = "Here is Some Text";
+            string someOtherText = "Here is some Text";
+            bool isEqual = someText.Equals(someOtherText, StringComparison.OrdinalIgnoreCase);
+            string formattedString = string.Format("{0}{1}Here is more text", someText, someOtherText);
+            Console.WriteLine(formattedString);
+            Console.WriteLine(someText.Length);
+            Console.WriteLine(someText[8]);
+            Console.WriteLine(someText.Substring(8, 4));
+            Console.WriteLine(someText.ToLower());
+            Console.WriteLine(someText.ToUpper());
+            string anotherText = string.Empty;
+            string replacedText = someText.Replace("text", "string");
+        }
+    }
+}
---
+++ BasicToAdvance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicToAdvance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation op = new Operation();
+            op.Oopeation();
+        }
+    }
+}
